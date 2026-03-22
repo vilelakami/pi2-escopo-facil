07c73fee15f7870e693f4e1071a498be341e5c34
Kami Vilela <vilelakamy18l@gmail.com>
2026-03-22 18:20:18 -0300

    feat: estilizando modal nova-tarefa

--- pages/tarefas/nova-tarefa.php
+++ pages/tarefas/nova-tarefa.php
@@ -10,57 +10,78 @@
             <h2>Nova Tarefa</h2>
         </div>
 
+        <!-- corpo do forms -->
         <div class="modal-body">
+
+            <!-- adicionando span pra mudar a cor do * -->
             <div class="modal-title">
-                <label>Título*</label>
+                <label>Título<span class="required">*</span></label>
                 <input type="text" name="taskTitle" id="taskTitle" placeholder="Ex: Criar tela de login">
             </div>
 
             <div class="modal-description">
-                <label>Descrição*</label>
+                <label>Descrição<span class="required">*</span></label>
                 <textarea name="taskDescription" id="taskDescription" placeholder="Descreva e detalhe o objetivo da tarefa"></textarea>
             </div>
 
+            <!-- separando prioridade e prazo inline -->
             <div class="modal-row">
-                <div class="modal-priority">
-                    <label>Prioridade*</label>
-                    <div class="modal-select">
-                        <img src="<?= BASE_URL ?>/assets/icon/grafico-baixa.svg" alt="baixa prioridade">
 
-                        <select class="task-priority" name="taskPriority" id="taskPriority">
+                <div class="modal-priority">
+                    <label>Prioridade<span class="required">*</span></label>
+
+                    <div class="modal-select">
+                        <img class="icon" src="<?= BASE_URL ?>/assets/icon/grafico-baixa.svg" alt="baixa prioridade">
+
+                        <select class="select-wrapper" name="taskPriority" id="taskPriority">
                             <option value="1">Baixa</option>
                             <option value="2">Média</option>
                             <option value="3">Alta</option>
 
                         </select>
                     </div>
+
                 </div>
 
                 <div class="modal-date">
-                    <label>Prazo*</label>
-                    <input type="date" name="taskDate" id="taskDate">
+                    <label>Prazo<span class="required">*</span></label>
+
+                    <div class="input-wrapper">
+                        <img class="icon" src="<?= BASE_URL ?>/assets/icon/calendar.svg" alt="calendário">
+                        <input type="date" name="taskDate" id="taskDate">
+                    </div>
+
                 </div>
+
             </div>
 
+            <!-- separando status -->
             <div class="modal-row">
+
                 <div class="modal-status">
-                    <label>Status*</label>
+                    <label>Status<span class="required">*</span></label>
 
-                    <img src="<?= BASE_URL ?>/assets/icon/a-fazer.svg" alt="a fazer">
+                    <div class="modal-select">
 
-                    <select name="taskStatus" id="taskStatus">
-                        <option value="1">A Fazer</option>
-                        <option value="2">Em Andamento</option>
-                        <option value="3">Concluído</option>
-                    </select>
+                        <img src="<?= BASE_URL ?>/assets/icon/a-fazer.svg" alt="status: a fazer" >
+
+                        <select class="select-wrapper" name="taskStatus" id="taskStatus">
+                            <option value="1">A Fazer</option>
+                            <option value="2">Em Andamento</option>
+                            <option value="3">Concluído</option>
+                        </select>
+                    </div>
+
                 </div>
-            </div>
 
-        </div>
+            </div>
+            
         <!-- footer -->
         <div class="modal-footer">
-            <button class="btn-cancel" id="closeModal">cancelar</button>
+            <button id="closeModal">Cancelar</button>
             <button class="btn-save" id="saveTask">+ Salvar Tarefa</button>
         </div>
+
     </div>
+
 </div>
